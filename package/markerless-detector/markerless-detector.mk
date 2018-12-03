dfc4bc3de19ab2a89d847bbbe92e20775c546e79
Vincent Rapido <vincent93400@hotmail.fr>
2018-12-03 22:11:55 +0100

    Annulation dernier push

--- package/markerless-detector/markerless-detector.mk
+++ package/markerless-detector/markerless-detector.mk
@@ -1,76 +1,53 @@
-# ################################################################################
-# #
-# # markerless-detector
-# #
-# ################################################################################
-
-# MARKERLESS_DETECTOR_SOURCE = master.tgz
-# MARKERLESS_DETECTOR_SITE = https://github.com/Chadys/MarkerlessDetector/archive/master.zip
-# MARKERLESS_DETECTOR_SETUP_TYPE = setuptools
-# MARKERLESS_DETECTOR_LICENSE = GNU
-
-# define MARKERLESS_DETECTOR_SETUP
-# 	( \
-# 		mkdir -p $(TARGET_DIR)/etc/markerless-detector; \
-# 		wget $(MARKERLESS_DETECTOR_SITE);
-# 		unzip ./master.zip
-# 		cp -r MarkerlessDetector-master \
-# 			$(TARGET_DIR)/etc/markerless-detector; \
-# 	)
-# endef
-
-# ifeq ($(BR2_PACKAGE_MARKERLESS_DETECTOR), y)
-# define MARKERLESS_DETECTOR_SETUP_DETECTOR
-# 	$(MARKERLESS_DETECTOR_SETUP_SIMPLE_MOTION_DETECTOR)
-# endef
-# endif
-
-# define MARKERLESS_DETECTOR_INSTALL_LOGGING_CFG
-# 	( \
-# 		mkdir -p $(TARGET_DIR)/etc/markerless-detector; \
-# 		mkdir -p $(TARGET_DIR)/root/markerless-detector-logs; \
-# 		cp $(@D)/example-configs/logging.ini \
-# 			$(TARGET_DIR)/etc/markerless-detector; \
-# 		sed -i s/args=\(\'logs.*/args=\(\'\\/root\\/markerless-detector-logs\\/log\',\'w\',100000,100\)/ \
-# 			$(TARGET_DIR)/etc/markerless-detector/logging.ini; \
-# 	)
-# endef
-
-# define MARKERLESS_DETECTOR_INSTALL_EXTRA
-# 	$(MARKERLESS_DETECTOR_INSTALL_LOGGING_CFG)
-# 	$(MARKERLESS_DETECTOR_SETUP)
-# endef
-
-# MARKERLESS_DETECTOR_POST_INSTALL_TARGET_HOOKS += MARKERLESS_DETECTOR_INSTALL_EXTRA
-
-# define MARKERLESS_DETECTOR_INSTALL_INIT_SYSV
-# 	$(INSTALL) -D -m 0755 $(@D)/markerless-detector \
-# 		$(TARGET_DIR)/etc/init.d
-# endef
-
-# $(eval $(python-package))
+################################################################################
 #
-# PHUTBALL
-# basic building rules
+# markerless-detector
 #
+################################################################################
 
-MARKERLESS_DETECTOR_VERSION = 1.1
-MARKERLESS_DETECTOR_SOURCE = master.tgz
-MARKERLESS_DETECTOR_SITE = NONE
+MARKERLESS_DETECTOR = 1.0
+MARKERLESS_DETECTOR_SOURCE = markerless-detector-$(MARKERLESS_DETECTOR).tar.gz
+MARKERLESS_DETECTOR_SITE = https://github.com/Chadys/MarkerlessDetector/archive/master.zip
+MARKERLESS_DETECTOR_SETUP_TYPE = setuptools
+MARKERLESS_DETECTOR_LICENSE = GNU
+MARKERLESS_DETECTOR_DEPENDENCIES = python-imutils
 
-MARKERLESS_DETECTOR_INSTALL_STAGING = YES
-
-define MARKERLESS_DETECTOR_BUILD_CMDS
-	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+define MARKERLESS_DETECTOR_SETUP
+	( \
+		mkdir -p $(TARGET_DIR)/etc/markerless-detector; \
+		wget $(MARKERLESS_DETECTOR_SITE);
+		unzip ./master.zip
+		cp -r MarkerlessDetector-master \
+			$(TARGET_DIR)/etc/markerless-detector; \
+	)
 endef
 
-define MARKERLESS_DETECTOR_INSTALL_STAGING_CMDS
-	$(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
+ifeq ($(BR2_PACKAGE_MARKERLESS_DETECTOR), y)
+define MARKERLESS_DETECTOR_SETUP_DETECTOR
+	$(MARKERLESS_DETECTOR_SETUP_SIMPLE_MOTION_DETECTOR)
+endef
+endif
+
+define MARKERLESS_DETECTOR_INSTALL_LOGGING_CFG
+	( \
+		mkdir -p $(TARGET_DIR)/etc/markerless-detector; \
+		mkdir -p $(TARGET_DIR)/root/markerless-detector-logs; \
+		cp $(@D)/example-configs/logging.ini \
+			$(TARGET_DIR)/etc/markerless-detector; \
+		sed -i s/args=\(\'logs.*/args=\(\'\\/root\\/markerless-detector-logs\\/log\',\'w\',100000,100\)/ \
+			$(TARGET_DIR)/etc/markerless-detector/logging.ini; \
+	)
 endef
 
-define MARKERLESS_DETECTOR_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/markerless-detector \
-						  $(TARGET_DIR)/etc/init.d
+define MARKERLESS_DETECTOR_INSTALL_EXTRA
+	$(MARKERLESS_DETECTOR_INSTALL_LOGGING_CFG)
+	$(MARKERLESS_DETECTOR_SETUP)
 endef
 
-$(eval $(markerless-detector-package))
+MARKERLESS_DETECTOR_POST_INSTALL_TARGET_HOOKS += MARKERLESS_DETECTOR_INSTALL_EXTRA
+
+define MARKERLESS_DETECTOR_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 755 package/markerless-detector/S50markerless-detector \
+		$(TARGET_DIR)/etc/init.d
+endef
+
+$(eval $(python-package))
